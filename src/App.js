c9a1d35d267363fa19931e0b6b3c71539f8ef79c
Meet Patel <m_patel175312@fanshaweonline.ca>
2023-04-26 18:58:43 -0400

    use API to update JSON file that contains the data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import "./index.css";
+import axios from "axios";
 
 function App() {
+
+  // State of Books Array
   const [book, setBook] = useState([]);
-  const handleBook = (title) => {
-    setBook([...book, { id: Math.round(Math.random() * 9999), title }]);
+
+   // Fatches all the Books
+  const fetchBooks = async() =>{
+    const response = await axios.get('http://localhost:3012/book');
+    setBook(response.data);
+  }
+
+  useEffect(()=>fetchBooks(),[]);
+
+   // Create a Book
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3012/book',{
+      title
+    });
+    setBook([...book, response.data ]);
   };
 
-  function deleteHandler(id) {
+  // Edit the Book
+  const editBookById=async (id, newTitle)=>{
+    const response = await axios.put(`http://localhost:3012/book/${id}`,{id,title:newTitle});
+    console.log(response);
+    const updatedBooks = book.map((eachBook)=>{
+      if(eachBook.id === id){
+        return {...eachBook,...response.data};
+      }
+      return eachBook;
+    })
+    setBook(updatedBooks);
+  }
+  
+ // Delete a Book
+  const deleteHandler= async(id)=> {
+    await axios.delete(`http://localhost:3012/book/${id}`);
+    
     const updatedArray = book.filter((eachBook) => {
       return eachBook.id !== id;
     });
     setBook(updatedArray);
   }
 
-  function editBookById(id, newTitle){
-    const updatedBooks = book.map((book)=>{
-      if(book.id === id){
-        return {...book,title:newTitle};
-      }
-      return book;
-    })
-    setBook(updatedBooks);
-  }
   return (
     <div className="app">
       <h1>Reading List</h1>
       <BookList bookArray={book} deleteBook={deleteHandler} editBook={editBookById}/>
-      <BookCreate addBook={handleBook} />
+      <BookCreate addBook={createBook} />
     </div>
   );
 }
